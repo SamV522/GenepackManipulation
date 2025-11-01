24850a71d3193d238a9ae17d979b50ea49f49ce3
Sam Vlahos <Sam.V_@hotmail.com>
2025-11-01 22:00:27 +1000

    * Tweak UI
    Update the Genepack manipulation dialog to make it more extensible

--- Source/GenepackManipulation/Dialogs/GenepackManipulationDialog.cs
+++ Source/GenepackManipulation/Dialogs/GenepackManipulationDialog.cs
@@ -2,7 +2,6 @@
 using GenepackManipulation.Components.World;
 using GenepackManipulation.Jobs.Data;
 using RimWorld;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -13,17 +12,22 @@ namespace GenepackManipulation.Dialogs
 {
     public class GenepackManipulationDialog : Window
     {
-        private readonly Manipulations.GenepackManipulation _genepackManipulation;
-        private readonly Building_GeneAssembler _assembler;
-        private readonly GenepackManipulatorComponent _genepackManipulatorComponent;
+        protected readonly Manipulations.GenepackManipulation _genepackManipulation;
+        protected readonly Building_GeneAssembler _assembler;
+        protected readonly GenepackManipulatorComponent _genepackManipulatorComponent;
 
         private Genepack _selectedGenepack;
-        private Vector2 _scrollPos;
-        private float _scrollHeight;
+        protected Vector2 _scrollPos;
+        protected float _scrollHeight;
 
-        public override Vector2 InitialSize => new Vector2(1016f, (float)UI.screenHeight / 2);
+        protected static readonly float GeneGap = 4f;
+        protected static readonly float GenepackGap = 14f;
+        protected static readonly float GenepackBiostatLabelWidth = 36f;
+        protected static readonly Vector2 GeneSize = new Vector2(87f, 68f);
 
-        internal GenepackManipulationDialog(Building_GeneAssembler assembler, Manipulations.GenepackManipulation genepackManipulation)
+        public override Vector2 InitialSize => new Vector2(UI.screenWidth * 0.525f, UI.screenHeight * 0.75f);
+
+        public GenepackManipulationDialog(Building_GeneAssembler assembler, Manipulations.GenepackManipulation genepackManipulation)
         {
             this._assembler = assembler;
             this._genepackManipulatorComponent = assembler.GetComp<GenepackManipulatorComponent>();
@@ -33,22 +37,107 @@ namespace GenepackManipulation.Dialogs
             this.closeOnClickedOutside = true;
         }
 
+        /// <summary>
+        /// Invoked when the genes of the entity have changed.
+        /// </summary>
+        /// <remarks>
+        /// <para>This method is called to handle any logic that should occur when the genes are modified.</para>
+        /// <para>Subclasses can override this method to implement custom behavior in response to gene changes.</para>
+        /// </list>
+        /// </remarks>
+        protected virtual void OnGenesChanged()
+        {
+            return;
+        }
+
+        /// <summary>
+        /// Invoked once the user clicks a Genepack button in the dialog.
+        /// </summary>
+        /// <remarks>Subclasses can override this method to implement custom behaviour.</remarks>
+        /// <param name="genepack">The genepack to be selected. Cannot be null.</param>
+        protected virtual void TrySelectGenepack(Genepack genepack)
+        {
+            _selectedGenepack = genepack;
+            SoundDefOf.Tick_High.PlayOneShotOnCamera();
+            OnGenesChanged();
+
+            return;
+        }
+
+        /// <summary>
+        /// Invoked when the user clicks the confirm button in the dialog.
+        /// </summary>
+        /// <remarks>Subclasses can override this method to implement custom behaviour.</remarks>
+        protected virtual void OnConfirmManipulation()
+        {
+
+                var targetGenepacks = new List<Genepack> { _selectedGenepack };
+                if (cooldowns.IsOnCooldown(targetGenepacks))
+                    {
+                        if (!cooldowns.IsOnCooldown(genepack))
+                        {
+                            int remainingTicks = cooldowns.GetRemainingTicks(genepack);
+                            float hours = (float)remainingTicks / GenDate.TicksPerHour;
+                            Messages.Message("GenepackManipulationOnCooldown".Translate(hours.ToString("0.0")), MessageTypeDefOf.RejectInput, false);
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    GenepackManipulationJobData jobData = new GenepackManipulationJobData()
+                    {
+                        Genepacks = targetGenepacks,
+                        Manipulation = _genepackManipulation,
+                        TicksRequired = targetGenepacks.Sum(g => g.GeneSet.ComplexityTotal) * 2500,
+                        RequiredIngredients = _genepackManipulation.CalculateRequiredIngredients(targetGenepacks)
+                    };
+
+                    _genepackManipulatorComponent.SetJob(jobData);
+
+                    Close();
+                }
+            }
+            else
+            {
+                Messages.Message("GenepackManipulationPleaseSelect".Translate(), MessageTypeDefOf.RejectInput, false);
+            }
+        }
+
+        /// <summary>
+        /// Draws a labeled section on the user interface and returns the rectangle representing the content area.
+        /// </summary>
+        /// <remarks>Subclasses can override this method to implement custom drawing logic for sections.</remarks>
+        /// <param name="rect">The rectangle defining the position and size of the section.</param>
+        /// <param name="label">The text label to display at the top of the section.</param>
+        /// <returns>A <see cref="Rect"/> representing the content area of the section, excluding the label area.</returns>
+        protected Rect DrawSection(Rect rect, string label)
+        {
+            Text.Anchor = TextAnchor.UpperLeft;
+            Widgets.Label(rect, label);
+            Rect contentRect = new Rect(rect.x, rect.y + Text.LineHeight, rect.width, rect.height - Text.LineHeight);
+            Widgets.DrawBoxSolid(contentRect, Widgets.MenuSectionBGFillColor);
+            return contentRect;
+        }
+
         public override void DoWindowContents(Rect inRect)
         {
-            Text.Font = GameFont.Medium;
-            Widgets.Label(new Rect(inRect.x, inRect.y, inRect.width, 40f), "GenepackManipulationSelect".Translate(_genepackManipulation.Verb));
-            Text.Font = GameFont.Small;
 
-            Rect scrollRect = new Rect(inRect.x, inRect.y + 50f, inRect.width, inRect.height - 100f);
+            Text.Font = GameFont.Medium;
+            Rect headerRect = new Rect(inRect.x, inRect.y, inRect.width, Text.LineHeight);
+            Widgets.Label(headerRect, "GenepackManipulationSelect".Translate(_genepackManipulation.Verb));
+
+            Rect sectionRect = new Rect(inRect.x + 10, headerRect.y + headerRect.height + GeneGap, inRect.width - 30, inRect.height - 100f);
+            Text.Font = GameFont.Small;
+            Rect contentRect = DrawSection(sectionRect, "GenepackLibrary".Translate());         
+            
+            Rect scrollRect = new Rect(contentRect.x +4f, contentRect.y, contentRect.width - 4f, contentRect.height);
             Rect viewRect = new Rect(0, 0f, scrollRect.width - 64f, _scrollHeight);
 
             Widgets.BeginScrollView(scrollRect, ref _scrollPos, viewRect);
 
-            float curX = 10f;
-            float curY = 10f;
-            float packWidth = 34;
-            float packHeight = 100f;
-            float verticalSpacing = 18f;
+            float curX = 4f;
+            float curY = 8f;
 
             IEnumerable<Genepack> genepacks = _genepackManipulation.FilterGenepacks(_assembler.GetGenepacks(true, true))
                                                         .OrderByDescending(genepack => genepack.GeneSet.ArchitesTotal)
@@ -57,106 +146,97 @@ namespace GenepackManipulation.Dialogs
 
             foreach (Genepack genepack in genepacks)
             {
-                packWidth = (float)(34.0 + (double)GeneCreationDialogBase.GeneSize.x * (double)genepack.GeneSet.GenesListForReading.Count + 4.0 * (double)(genepack.GeneSet.GenesListForReading.Count + 2));
-                Rect packRect = new Rect(curX, curY, packWidth, packHeight);
-                if (DrawGenepack(genepack,ref curX, curY, packRect))
+                var thisPackWidth = GenepackBiostatLabelWidth +
+                                        (GeneSize.x * genepack.GeneSet.GenesListForReading.Count) +
+                                        (GeneGap * (genepack.GeneSet.GenesListForReading.Count + 1));
+
+                Rect packRect = new Rect(curX, curY, thisPackWidth, GeneSize.y + (GeneGap * 2));
+
+                if (DrawGenepackButton(genepack,ref curX, curY, packRect))
                 {
-                    _selectedGenepack = genepack;
-                    SoundDefOf.Tick_High.PlayOneShotOnCamera();
+                    TrySelectGenepack(genepack);
                 }
 
-                curX += 4;
-                if (curX + packWidth > viewRect.width)
+                curX += GeneGap;
+                if (curX + thisPackWidth > viewRect.width)
                 {
-                    curX = 10f;
-                    curY += packHeight + verticalSpacing;
+                    curX = GeneGap;
+                    curY += packRect.height + GenepackGap;
                 }
             }
 
             if (Event.current.type == EventType.Layout)
-                _scrollHeight = curY + packHeight + verticalSpacing;
+                _scrollHeight = curY + GeneSize.y + (GeneGap * 2) + GenepackGap;
 
             Widgets.EndScrollView();
 
             // Confirm button button
             if (Widgets.ButtonText(new Rect(inRect.xMax - 158f, inRect.yMax - 40f, 150f, 30f), "GenepackManipulationStart".Translate(_genepackManipulation.Gerund.CapitalizeFirst())))
             {
-                if (_selectedGenepack != null)
-                {
-                    var targetGenepacks = new List<Genepack> { _selectedGenepack };
-                    var cooldowns = Find.World.GetComponent<GenepackCooldownWorldComponent>();
-                    if (cooldowns.IsOnCooldown(targetGenepacks))
-                    {
-                        foreach (Genepack genepack in targetGenepacks.Where(x => x != null))
-                        {
-                            if (!cooldowns.IsOnCooldown(genepack))
-                            {
-                                int remainingTicks = cooldowns.GetRemainingTicks(genepack);
-                                float hours = (float)remainingTicks / GenDate.TicksPerHour;
-                                Messages.Message("GenepackManipulationOnCooldown".Translate(hours.ToString("0.0")), MessageTypeDefOf.RejectInput, false);
-                                return;
-                            }
-                        }
-                    }
-                    else
-                    {
-
-
-                        GenepackManipulationJobData jobData = new GenepackManipulationJobData()
-                        {
-                            Genepacks = targetGenepacks,
-                            Manipulation = _genepackManipulation,
-                            TicksRequired = targetGenepacks.Sum(g => g.GeneSet.ComplexityTotal) * 2500,
-                            RequiredIngredients = _genepackManipulation.CalculateRequiredIngredients(targetGenepacks)
-                        };
-
-                        _genepackManipulatorComponent.SetJob(jobData);
-
-                        Close();
-                    }                        
-                }
-                else
-                {
-                    Messages.Message("GenepackManipulationPleaseSelect".Translate(), MessageTypeDefOf.RejectInput, false);
-                }
+                OnConfirmManipulation();
             }
 
-            if (Widgets.ButtonText(new Rect(8, inRect.yMax - 40f, 150f, 30f), "Close".Translate()))
+            // Close button
+            if (Widgets.ButtonText(new Rect(0, inRect.yMax - 40f, 150f, 30f), "Close".Translate()))
             {
                 Close();
             }
         }
 
-        private bool DrawGenepack(Genepack genepack,
+        /// <summary>
+        /// Determines whether the specified genepack is currently selected.
+        /// </summary>
+        /// <remarks>Subclasses can override this method to implement custom selection validation logic.</remarks>
+        /// <param name="genepack">The genepack to check for selection.</param>
+        /// <returns><see langword="true"/> if the specified genepack is selected; otherwise, <see langword="false"/>.</returns>
+        protected virtual bool IsGenepackSelected(Genepack genepack)
+        {
+            return _selectedGenepack == genepack;
+        }
+
+        /// <summary>
+        /// Draws a button representing a genepack and handles user interaction with it.
+        /// </summary>
+        /// <remarks>
+        /// <para>The button visually represents the properties of <paramref name="genepack"/> and allows the user to
+        /// interact with it. The method updates the <paramref name="curX"/> position to account for the drawn button
+        /// and it's contents.</para>
+        /// <para>Subclasses can override this method to implement custom drawing logic for genepack buttons.</para>
+        /// </remarks>
+        /// <param name="genepack">The genepack to be represented by the button.</param>
+        /// <param name="curX">The current X position, which will be updated after drawing.</param>
+        /// <param name="curY">The Y position where the button should be drawn.</param>
+        /// <param name="rect">The rectangle defining the button's position and size.</param>
+        /// <returns><see langword="true"/> if the button was clicked; otherwise, <see langword="false"/>.</returns>
+        protected virtual bool DrawGenepackButton(Genepack genepack,
               ref float curX,
               float curY,
               Rect rect)
         {
             bool clicked = false;
 
-            Widgets.DrawBoxSolid(rect, genepack == _selectedGenepack ? new Color(0.3f, 0.6f, 0.3f, 0.3f) : new Color(0.2f, 0.2f, 0.2f, 0.2f));
-            Widgets.DrawBox(rect);
-            Widgets.DrawHighlight(rect);
-            Widgets.DrawBox(rect);
+            Widgets.DrawBoxSolidWithOutline(rect, IsGenepackSelected(genepack) ? new Color(0.3f, 0.6f, 0.3f, 0.3f) : new Color(1f, 1f, 1f, 0.1f), new Color(0.5f, 0.5f, 0.5f, 0.5f));
             GUI.color = Color.white;
+            curX += GeneGap;
             GeneUIUtility.DrawBiostats(genepack.GeneSet.ComplexityTotal, genepack.GeneSet.MetabolismTotal, genepack.GeneSet.ArchitesTotal, ref curX, curY, 4f);
             List<GeneDef> genesListForReading = genepack.GeneSet.GenesListForReading;
             for (int index = 0; index < genesListForReading.Count; ++index)
             {
                 GeneDef gene = genesListForReading[index];
-                Rect geneRect = new Rect(curX, curY + 4f, GeneCreationDialogBase.GeneSize.x, GeneCreationDialogBase.GeneSize.y);
-                string extraTooltip = (string)null;
-                GeneUIUtility.DrawGeneDef(genesListForReading[index], geneRect, GeneType.Xenogene, (Func<string>)(() => extraTooltip), false, false);
-                curX += GeneCreationDialogBase.GeneSize.x + 4f;
+                Rect geneRect = new Rect(curX, curY + GeneGap, GeneSize.x, GeneSize.y);
+                GeneUIUtility.DrawGeneDef(genesListForReading[index], geneRect, GeneType.Xenogene, null, false, false);
+                curX += GeneSize.x + GeneGap;
             }
-            Widgets.InfoCardButton(rect.xMax - 24f, rect.y + 2f, (Thing)genepack);
+
+            Widgets.InfoCardButton(rect.xMax - 24f, rect.y + 2f, genepack);
+
             if (Mouse.IsOver(rect))
                 Widgets.DrawHighlight(rect);
 
             if (Widgets.ButtonInvisible(rect))
                 clicked = true;
 
-            curX = Mathf.Max(curX, rect.xMax + 14f);
+            curX = Mathf.Max(curX, rect.xMax + GenepackGap);
 
             return clicked;
         }
